f2a2a6f0bf2de03d7c4f738d00b8d2091ef648fd
Manver Singh <manveersingh0501l@gmail.com>
2026-05-14 16:24:55 +0530

    fix forgot password

--- app/Http/Controllers/Auth/PasswordResetLinkController.php
+++ app/Http/Controllers/Auth/PasswordResetLinkController.php
@@ -30,16 +30,30 @@ class PasswordResetLinkController extends Controller
             'email' => ['required', 'email'],
         ]);
 
-        // We will send the password reset link to this user. Once we have attempted
-        // to send the link, we will examine the response then see the message we
-        // need to show to the user. Finally, we'll send out a proper response.
-        $status = Password::sendResetLink(
-            $request->only('email')
-        );
+        try {
+            // We will send the password reset link to this user. Once we have attempted
+            // to send the link, we will examine the response then see the message we
+            // need to show to the user. Finally, we'll send out a proper response.
+            $status = Password::sendResetLink(
+                $request->only('email')
+            );
 
-        return $status == Password::RESET_LINK_SENT
-                    ? back()->with('status', __($status))
-                    : back()->withInput($request->only('email'))
-                        ->withErrors(['email' => __($status)]);
+            return $status == Password::RESET_LINK_SENT
+                        ? back()->with('status', __($status))
+                        : back()->withInput($request->only('email'))
+                            ->withErrors(['email' => __($status)]);
+                            
+        } catch (\Exception $e) {
+            // Log the full error for production diagnosis
+            \Log::error("Password Reset Link Failure: " . $e->getMessage(), [
+                'email' => $request->email,
+                'trace' => $e->getTraceAsString()
+            ]);
+
+            // Provide a graceful fallback to the user
+            return back()
+                ->withInput($request->only('email'))
+                ->withErrors(['email' => 'We encountered an error while sending the reset link. Please try again later or contact support.']);
+        }
     }
 }
